506f4266aa4348b201eb29b0eb2264041d74e0fe
deep.patel <deep.patel@openlane.com>
2025-01-03 14:02:16 -0500

    add customers

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -10,4 +10,18 @@ public class MoviesController : Controller
         var movie = new Movie() {Name = "Shrek"};
         return View(movie);
     }
+    
+    public ViewResult Index()
+    {
+        var movies = GetMovies();
+        return View(movies);    
+    }
+    private IEnumerable<Movie> GetMovies()
+    {
+        return new List<Movie>
+        {
+            new Movie { Id = 1, Name = "Shrek" },
+            new Movie { Id = 2, Name = "Wall-e" }
+        };
+    }
 }
